5833eec350e4ac3b49bab9586744ef460d220593
Arlid Henao Rueda <arlidhenaorueda@gmail.com>
2024-02-17 14:38:28 -0500

    Initial Commit

---
+++ blockphonecallsapp/blockphonecallsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockphonecallsappApp.swift
+//  blockphonecallsapp
+//
+//  Created by Arlid Henao Rueda on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct blockphonecallsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
